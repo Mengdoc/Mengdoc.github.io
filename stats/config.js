0b3e0f69a296e56f819c3877517777a39e3a8fd5
Mengdoc <2317113278@qq.com>
2024-02-09 17:46:25 +0800

    update

---
+++ stats/config.js
@@ -0,0 +1,37 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '梦泽の监控',
+  SiteUrl: 'https://mengze2.cn/stats',
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur2330848-2ff8329e9aac9d516aa4f5ee',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '博客',
+      url: 'https://mengze2.cn/'
+    },
+    {
+      text: '硅云',
+      url: 'https://vpsor.cn/'
+    },
+    {
+      text: '文档',
+      url: 'https://mengdoc.github.io/'
+    },
+    {
+      text: 'github',
+      url: 'https://github.com/MengZeMC/'
+    },
+  ],
+};
